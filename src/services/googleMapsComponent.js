ab0b778b2473850760eb69ccf302b6c6de93479c
waj949 <wajdihamrouni1993@gmail.com>
2020-02-05 10:51:09 +0100

    front

--- src/services/googleMapsComponent.js
+++ src/services/googleMapsComponent.js
@@ -4,8 +4,8 @@ export default {
   searchPharmacies: query => {
     // console.log query
     return axios
-      .post(`http://localhost:5000/api/pharmacy/search`, { query }) // the proxy isn't working properly
+      .post(`http://localhost:3001/api/pharmacy/search`, { query }) // the proxy isn't working properly
       .then(res => res.data)
-      .catch(err => console.log("err in service ", err.response));
+      .catch(err => console.log("err in service ", err));
   }
 };
